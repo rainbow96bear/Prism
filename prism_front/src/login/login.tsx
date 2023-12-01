8c147b521bf47387b09dc839ca2421def895a5ee
KimSungJin <rainbow96bear@gmail.com>
2023-12-01 17:20:49 +0900

    front login component

---
+++ prism_front/src/login/login.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Login = () => {
+  const KakaoLogin = async () => {
+    // await axios.
+  };
+  return <div>Loading...</div>;
+};
+
+export default Login;
