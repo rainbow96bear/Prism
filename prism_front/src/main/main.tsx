0b046fbb362b74b105813109fcc080b55e21c06f
KimSungJin <rainbow96bear@gmail.com>
2023-12-01 16:25:02 +0900

    home, community, project 구분을 위한 component 생성

---
+++ prism_front/src/main/main.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Main = () => {
+  return <MainContainer>asdf asdf</MainContainer>;
+};
+
+export default Main;
+
+const MainContainer = styled.div`
+  height: 100%;
+`;
