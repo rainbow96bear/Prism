028b97eec45fcf19eaed25bece00e7deb1e44a56
KimSungJin <rainbow96bear@gmail.com>
2023-12-02 08:42:10 +0900

    로그인 버튼 카카오 로그인으로 변경

--- prism_front/src/header/Components/beforeLogin.tsx
+++ prism_front/src/header/Components/beforeLogin.tsx
@@ -1,37 +1,29 @@
 import styled from "styled-components";
 
-interface AfterLogin {
-  type: string;
-}
-
-const darkSkyBlue = "rgba(0, 102, 204, 0.8)";
+import loginImg from "./../../assets/kakao_login_small.png";
 
 const AfterLogin: React.FC = () => {
-  const Rest_api_key = "b56eb8d850568962a66cb7b117530c3c"; //REST API KEY
-  const redirect_uri = "http://localhost:3000/auth"; //Redirect URI
+  const Rest_api_key = process.env.REACT_APP_REST_API_KEY;
+  const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
   // oauth 요청 URL
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
   const handleLogin = () => {
     window.location.href = kakaoURL;
+    console.log(window.location.href);
   };
   https: return (
     <>
-      <button onClick={handleLogin}>카카오 로그인</button>
-      <ButtomBox type="login">로그인</ButtomBox>
-      <ButtomBox type="signUp">회원가입</ButtomBox>
+      <ButtomBox onClick={handleLogin}>
+        <img src={loginImg} />
+      </ButtomBox>
     </>
   );
 };
 
 export default AfterLogin;
 
-const ButtomBox = styled.div<AfterLogin>`
+const ButtomBox = styled.div`
   height: 50%;
   margin: 0px 10px;
   cursor: pointer;
-  font-weight: bold;
-  padding: 3px 7px;
-  border-radius: 7px;
-  background-color: ${({ type }) =>
-    type === "login" ? "lightblue" : darkSkyBlue};
 `;
