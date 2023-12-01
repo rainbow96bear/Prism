6b0a1e13620243bba2952d363ddaeaac56f4c4a8
KimSungJin <rainbow96bear@gmail.com>
2023-12-01 16:24:23 +0900

    BeforeLogin과 AfterLogin으로 나누어 관리

--- prism_front/src/header/Components/funcBar.tsx
+++ prism_front/src/header/Components/funcBar.tsx
@@ -1,24 +1,13 @@
 import styled from "styled-components";
-import { FaRegBell } from "react-icons/fa";
-import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
-import { FaRegEdit } from "react-icons/fa";
-import { BsPersonCircle } from "react-icons/bs";
+
+import BeforeLogin from "./afterLogin";
+import AfterLogin from "./beforeLogin";
 
 const FuncBar: React.FC = () => {
   return (
     <Box>
-      <ButtomBox>
-        <FaRegBell size={"100%"} />
-      </ButtomBox>
-      <ButtomBox>
-        <IoChatbubbleEllipsesOutline size={"100%"} />
-      </ButtomBox>
-      <ButtomBox>
-        <FaRegEdit size={"100%"} />
-      </ButtomBox>
-      <ButtomBox>
-        <BsPersonCircle size={"100%"} />
-      </ButtomBox>
+      {/* <BeforeLogin /> */}
+      <AfterLogin />
     </Box>
   );
 };
@@ -30,11 +19,4 @@ const Box = styled.div`
   justify-content: right;
   height: 100%;
   align-items: center;
-`;
-const ButtomBox = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 0px 10px;
-  cursor: pointer;
-  height: 60%;
 `;
