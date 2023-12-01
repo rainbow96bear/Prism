38340124cf1b5ae4138f04306d71b2a135ae4948
KimSungJin <rainbow96bear@gmail.com>
2023-12-01 16:23:17 +0900

    CSS 틀 수정

--- prism_front/src/App.tsx
+++ prism_front/src/App.tsx
@@ -1,11 +1,20 @@
 import styled from "styled-components";
 
 import Header from "./header/header";
+import Main from "./main/main";
+import Login from "./login/login";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <Box>
-      <Header></Header>
+      <Route path="/kakaoLogin" Component={Login}></Route>
+      <HeaderArea>
+        <Header></Header>
+      </HeaderArea>
+      <BodyArea>
+        <Main></Main>
+      </BodyArea>
     </Box>
   );
 }
@@ -15,4 +24,17 @@ export default App;
 const Box = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+`;
+const HeaderArea = styled.div`
+  width: 100%;
+  height: 55px;
+  border-bottom: 2px solid gray;
+`;
+
+const BodyArea = styled.div`
+  width: 100%;
+  max-width: 70%;
+  height: auto;
+  background-color: blue;
 `;
