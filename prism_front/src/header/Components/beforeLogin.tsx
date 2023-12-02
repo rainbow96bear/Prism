5981a2d97d6b8a92a033feb798d7e8a35c76057c
KimSungJin <rainbow96bear@gmail.com>
2023-12-02 22:18:06 +0900

    카카오 로그인 버튼 구현

--- prism_front/src/header/Components/beforeLogin.tsx
+++ prism_front/src/header/Components/beforeLogin.tsx
@@ -3,13 +3,9 @@ import styled from "styled-components";
 import loginImg from "./../../assets/kakao_login_small.png";
 
 const AfterLogin: React.FC = () => {
-  const Rest_api_key = process.env.REACT_APP_REST_API_KEY;
-  const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
   // oauth 요청 URL
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
   const handleLogin = () => {
-    window.location.href = kakaoURL;
-    console.log(window.location.href);
+    window.location.href = "http://localhost:8080/kakaoLogin";
   };
   https: return (
     <>
