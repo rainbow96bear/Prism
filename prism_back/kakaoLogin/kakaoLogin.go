fff335e4f6a813b1935434197ea1573a24146765
KimSungJin <rainbow96bear@gmail.com>
2023-12-04 10:00:10 +0900

    기능 별 함수 생성

--- prism_back/kakaoLogin/kakaoLogin.go
+++ prism_back/kakaoLogin/kakaoLogin.go
@@ -2,9 +2,11 @@ package KakaoLogin
 
 import (
 	"bytes"
+	"encoding/json"
+	"time"
+
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -12,28 +14,47 @@ import (
 	"github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
 )
+
+// oauth token 정보
+
+
 var SECRET_KEY string = os.Getenv("SECRET_KEY")
 var store = sessions.NewCookieStore([]byte(SECRET_KEY))
 
-func GetAuthorize(res http.ResponseWriter, req *http.Request) {
-	err := godotenv.Load(".env")
-    
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+var err = godotenv.Load(".env")
+
+//OAuthLogin 시작
+func OAuthLogin(res http.ResponseWriter, req *http.Request) {
 	REST_API_KEY := os.Getenv("REST_API_KEY")
 	REDIRECT_URI := os.Getenv("REDIRECT_URI")
-	fmt.Println("REST_API_KEY",REST_API_KEY)
-	fmt.Println("REDIRECT_URI",REDIRECT_URI)
-	redirectURL := fmt.Sprintf("https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=%s&redirect_uri=%s",
-	REST_API_KEY, REDIRECT_URI)
-	fmt.Println("redirectURL",redirectURL)
+	var redirectURL string
+	redirectURL = fmt.Sprintf("https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=%s&redirect_uri=%s",
+		REST_API_KEY, REDIRECT_URI)
 
 	http.Redirect(res, req, redirectURL, http.StatusFound)
 }
+//user정보 처리
+func OAuthLoginAfterProcess(res http.ResponseWriter, req *http.Request){
+	//
+	token, err := GetToken(res, req)
+	if err != nil {
+		fmt.Println("token 획득 실패 : ", err)
+	}
+	// Access_token을 이용한 user 정보 받기
+	user, err := GetUserInfo(token.Access_token)
+	if err != nil {
+		fmt.Println("정보 획득 실패: ", err)
+	}
+	fmt.Println("user정보 확인", user)
+	// json.NewEncoder(res).Encode(user)
+	MakeCookie(res, user)
+	http.Redirect(res, req, "http://localhost:3000/home", http.StatusFound)
+}
 
-func GetToken(res http.ResponseWriter, req *http.Request) {
-
+// 토큰 가져오기
+func GetToken(res http.ResponseWriter, req *http.Request) (Token, error) {
+	// 반환할 token
+	var token Token
 	// 사용할 클라이언트 ID, 리다이렉트 URI, 클라이언트 시크릿, 토큰 요청 URI 등을 설정
 	CLIENT_SECRET_KEY := os.Getenv("CLIENT_SECRET_KEY")
 	REST_API_KEY := os.Getenv("REST_API_KEY")
@@ -56,7 +77,7 @@ func GetToken(res http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		fmt.Println("Error creating request:", err)
 		http.Error(res, "Internal Server Error", http.StatusInternalServerError)
-		return
+		return token, err
 	}
 
 	// 요청 헤더 설정
@@ -68,29 +89,66 @@ func GetToken(res http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		fmt.Println("Error making request:", err)
 		http.Error(res, "Internal Server Error", http.StatusInternalServerError)
-		return
+		return token, err
 	}
 	defer resp.Body.Close()
-
-	fmt.Println("Response Status Code:", resp.Status)
 
 	// HTTP 응답 본문을 문자열로 읽어오기
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error reading response body:", err)
 		http.Error(res, "Internal Server Error", http.StatusInternalServerError)
-		return
+		return token, err
 	}
 
 	// HTTP 응답 본문 출력
-	fmt.Println("Response Body:", string(body))
-	// 응답 처리 및 세션에 토큰 저장
-	// ...
 
-	// 클라이언트 리디렉션
-	http.Redirect(res, req, "http://localhost:3000/home", http.StatusFound)
+	err = json.Unmarshal(body, &token)
+	if err != nil {
+		fmt.Println("JSON 파싱 오류:", err)
+		return token, err
+	}
+
+	return token, nil
 }
 
-// func GetUserInfo(res http.ResponseWriter, req *http.Request) {
+// user정보 가져오기
+func GetUserInfo(Access_token string) (User, error){
+	var user User
+	// requestURL := "https://kapi.kakao.com/v2/user/me"
+	requestURL := "https://kapi.kakao.com/v1/oidc/userinfo"
 
-// }
+	req, err := http.NewRequest("GET", requestURL, nil)
+	if err != nil {
+		return user, fmt.Errorf("Error creating request: %v\n", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+Access_token)
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return user, fmt.Errorf("UserInfo 얻기 오류: %v\n", err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	fmt.Println(string(body))
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		return user, fmt.Errorf("Unmarshal 오류 : %v\n", err)
+	}
+
+	return user, nil
+}
+
+// cookie 만들기
+func MakeCookie(res http.ResponseWriter, user User) {
+	cookie := http.Cookie{
+		Name : "kakaoLogin",
+		Value : fmt.Sprintf("%s",user.ID),
+		Expires: time.Now().Add(30 * 24 * time.Hour),
+		Path:    "/",
+		HttpOnly: true,
+	}
+	http.SetCookie(res, &cookie)
+}
