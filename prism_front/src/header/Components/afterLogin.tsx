336f2f72d7d135513ec537fb480ad9ba8bf65327
KimSungJin <rainbow96bear@gmail.com>
2023-12-05 18:04:44 +0900

    DropDown 적용

--- prism_front/src/header/Components/afterLogin.tsx
+++ prism_front/src/header/Components/afterLogin.tsx
@@ -2,53 +2,86 @@ import styled from "styled-components";
 import { FaRegBell } from "react-icons/fa";
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 import { FaRegEdit } from "react-icons/fa";
-import { BsPersonCircle } from "react-icons/bs";
 import { useEffect, useState } from "react";
+
+import DropDown from "../../CustomComponent/DropDown";
+import { TitlePath } from "../../GlobalType/TitlePath";
 
 const BeforeLogin: React.FC = () => {
   const [imgSrc, setImgSrc] = useState("");
+  const [profilePath, setProfilePath] = useState("");
+  const [dropdown, setDropdown] = useState(false);
 
   useEffect(() => {
-    // 쿠키에서 Img의 값 가져오기
+    // 쿠키에서 Img 및 ID 값 가져오기
     const kakaoUserValue = document.cookie;
     const newstring = kakaoUserValue.substring(1, kakaoUserValue.length - 1);
     const imgMatch = newstring.match(/Img=([^,]+)/);
     const imgValue = imgMatch ? decodeURIComponent(imgMatch[1]) : "";
 
+    const IdMatch = newstring.match(/ID=([^,]+)/);
+    const userIDValue = IdMatch ? decodeURIComponent(IdMatch[1]) : "";
+
     // Img의 값이 있다면 state에 저장
     if (imgValue) {
       setImgSrc(imgValue);
     }
+
+    // ID 값이 있다면 profilePath를 생성하여 state에 저장
+    if (userIDValue) {
+      setProfilePath(`/profile/${userIDValue}`);
+    }
   }, []);
+
+  const DropDown_List: TitlePath[] = [{ title: "프로필", path: profilePath }];
+
   return (
     <>
-      <ButtomBox>
+      <ButtonBox>
         <FaRegBell size={"100%"} />
-      </ButtomBox>
-      <ButtomBox>
+      </ButtonBox>
+      <ButtonBox>
         <IoChatbubbleEllipsesOutline size={"100%"} />
-      </ButtomBox>
-      <ButtomBox>
+      </ButtonBox>
+      <ButtonBox>
         <FaRegEdit size={"100%"} />
-      </ButtomBox>
-      <ButtomBox>
-        {imgSrc == "" ? (
-          <BsPersonCircle size={"100%"} />
+      </ButtonBox>
+      <ButtonBox>
+        <ProfileImg
+          onClick={() => setDropdown(!dropdown)}
+          src={imgSrc}
+          alt="User Profile"
+        />
+
+        {dropdown ? (
+          <S_DropDown onClick={() => setDropdown(!dropdown)}>
+            <DropDown list={DropDown_List} setDropdown={setDropdown}></DropDown>
+          </S_DropDown>
         ) : (
-          <ProfileImg src={imgSrc} alt="User Profile" />
+          <></>
         )}
-      </ButtomBox>
+      </ButtonBox>
     </>
   );
 };
 
 export default BeforeLogin;
 
-const ButtomBox = styled.div`
+const ButtonBox = styled.div`
+  position: relative;
   height: 50%;
   margin: 0px 10px;
   cursor: pointer;
 `;
+
 const ProfileImg = styled.img`
   height: 100%;
+`;
+
+const S_DropDown = styled.div`
+  width: 100px;
+  position: absolute;
+  right: 0px;
+  top: 50px;
+  margin: 10px 0px;
 `;
