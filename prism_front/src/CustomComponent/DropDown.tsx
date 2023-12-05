32637a794624cd5195c6ec5c27e0d61b2f3e6619
KimSungJin <rainbow96bear@gmail.com>
2023-12-05 18:03:42 +0900

    DropDown 외에 클릭시 사라지도록 설정

--- prism_front/src/CustomComponent/DropDown.tsx
+++ prism_front/src/CustomComponent/DropDown.tsx
@@ -1,19 +1,41 @@
 import styled from "styled-components";
-
+import { useEffect, useRef } from "react";
 import { TitlePath } from "../GlobalType/TitlePath";
 import { useNavigate } from "react-router-dom";
 
 interface ComponentProps {
+  setDropdown: React.Dispatch<React.SetStateAction<boolean>>;
   list: TitlePath[];
 }
 
-const DropDown: React.FC<ComponentProps> = ({ list }) => {
+const DropDown: React.FC<ComponentProps> = ({ list, setDropdown }) => {
   const navigate = useNavigate();
   const move = (path: string) => {
     navigate(path);
+    setDropdown(false); // 클릭한 후에 드롭다운을 닫습니다.
   };
+
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setDropdown(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <Box>
+    <Box ref={dropdownRef}>
       {list.map((item, index) => (
         <Item
           key={"dropdown" + index}
