ffa3cf2a5858dee43d458a083ec6512640221200
KimSungJin <rainbow96bear@gmail.com>
2023-12-09 08:51:09 +0900

    테스트 코드 작업

--- prism_back/kakaoLogin/kakaoLogin_test.go
+++ prism_back/kakaoLogin/kakaoLogin_test.go
@@ -20,25 +20,3 @@ func TestGetToken(t * testing.T) {
 	}
 	fmt.Println("테스트 결과 : ",token)
 }
-
-func TestGetUserInfo(t *testing.T) {
-	access_token := "" 
-	user, err := GetUserInfo(access_token)
-	if err != nil {
-		fmt.Println(err)
-	}
-	expectedUserID := ""
-	
-	if user.NickName == expectedUserID {
-		fmt.Println("성공")
-		fmt.Println(user)
-	}
-}
-
-
-	
-	// KakaoLogin의 작업
-	//KakaoLogin이 'https://kauth.kakao.com/oauth/token'이 URL로 AUTHORIZE_CODE과REST_API_KEY를 전달 REDIRECT_URI도 포함
-	// Access Token을 GET 방식으로 "https://kapi.kakao.com/v2/user/me"에 전달 
-	// 	curl -v -X GET "https://kapi.kakao.com/v2/user/me" \
-	//   -H "Authorization: Bearer ${ACCESS_TOKEN}"
